59108ceec29bb354b87adf0f0227e296fed337cb
Jean Pablo Dreyer <djean@tcheturbo.local>
2022-07-07 15:37:54 -0300

    Hint

--- js/index_szchat.js
+++ js/index_szchat.js
@@ -48,34 +48,44 @@ const listUsers = async (vDATAC) => {
     users.forEach((user, index) => {
 
         aux = ``;
-        if (user.mensagem_id==1) //COBRANÇA
-            aux = `<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#FF0000' class='bi bi-calendar-x' viewBox='0 0 16 16'>
+        if (user.mensagem_id==1){ //COBRANÇA
+            auxtitulomarcador = `Cobrança`;
+            aux = `<svg xmlns='http://www.w3.org/2000/svg' alt='Cobrança' width='16' height='16' fill='#FF0000' class='bi bi-calendar-x' viewBox='0 0 16 16'>
             <path d='M6.146 7.146a.5.5 0 0 1 .708 0L8 8.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 9l1.147 1.146a.5.5 0 0 1-.708.708L8 9.707l-1.146 1.147a.5.5 0 0 1-.708-.708L7.293 9 6.146 7.854a.5.5 0 0 1 0-.708z'/>
-            <path d='M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z'/></svg>`
-        else if (user.mensagem_id==2) //BOAS VINDAS
-            aux = `<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#239B56' class='bi bi-house-heart-fill' viewBox='0 0 16 16'>
+            <path d='M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z'/><title>Cobrança</title></svg>`;
+        }    
+        else if (user.mensagem_id==2){ //BOAS VINDAS
+            auxtitulomarcador = `Boas Vindas - Internet`;
+            aux = `<svg xmlns='http://www.w3.org/2000/svg' alt='Boas Vindas - Internet' width='16' height='16' fill='#239B56' class='bi bi-house-heart-fill' viewBox='0 0 16 16'>
             <path d='M7.293 1.5a1 1 0 0 1 1.414 0L11 3.793V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v3.293l2.354 2.353a.5.5 0 0 1-.708.707L8 2.207 1.354 8.853a.5.5 0 1 1-.708-.707L7.293 1.5Z'/>
-            <path d='m14 9.293-6-6-6 6V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9.293Zm-6-.811c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.691 0-5.018Z'/></svg>`
-        else if (user.mensagem_id==7) //BOAS VINDAS MOVEL
+            <path d='m14 9.293-6-6-6 6V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9.293Zm-6-.811c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.691 0-5.018Z'/><title>Boas Vindas</title></svg>`;
+        }
+        else if (user.mensagem_id==7){ //BOAS VINDAS MOVEL
+            auxtitulomarcador = `Boas Vindas - Móvel`;
             aux = `<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#239B56' class='bi bi-chat-heart-fill' viewBox='0 0 16 16'>
-            <path d='M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9.06 9.06 0 0 0 8 15Zm0-9.007c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z'/></svg>`
-        else if (user.mensagem_id==3) //AVISO BLOQUEIO
-            aux = `<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#FB5B00' class='bi bi-bell-fill' viewBox='0 0 16 16'><path d='M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z'/></svg>`            
-        else if (user.mensagem_id==4) //SERASA SPC
+            <path d='M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9.06 9.06 0 0 0 8 15Zm0-9.007c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z'/><title>Boas Vindas - Móvel</title></svg>`;
+        }
+        else if (user.mensagem_id==3) { //AVISO BLOQUEIO
+            auxtitulomarcador = `Aviso de Bloqueio`;
+            aux = `<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#FB5B00' class='bi bi-bell-fill' viewBox='0 0 16 16'><path d='M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z'/><title>Aviso de Bloqueio</title></svg>`;
+        }
+        else if (user.mensagem_id==4){ //SERASA SPC
+            auxtitulomarcador = `SERASA/SPC`;
             aux = `<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#FF0000' class='bi bi-calendar-minus-fill' viewBox='0 0 16 16'>
-            <path d='M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zM6 10h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1 0-1z'/></svg>`            
+            <path d='M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zM6 10h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1 0-1z'/><title>SERASA/SPC</title></svg>`;
+        }
 
         auxsend = ``;
         auxbgcolor = ``;
         if (user.status==200) {//SUCESSO
             auxbgcolor = `table-light text-primary`;
             auxsend = `<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#2AFF00' class='bi bi-check-all' viewBox='0 0 16 16'>
-            <path d='M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992a.252.252 0 0 1 .02-.022zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486-.943 1.179z'/></svg>`
+            <path d='M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992a.252.252 0 0 1 .02-.022zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486-.943 1.179z'/><title>Sucesso</title></svg>`;
         }
         else {
             auxbgcolor = `table-secondary text-danger`; 
             auxsend = `<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='#c90a02' class='bi bi-check' viewBox='0 0 16 16'>
-            <path d='M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z'/></svg>`;
+            <path d='M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z'/><title>Não Enviado</title></svg>`;
         }
 
         /*if (user.frt_id != null || user.frt_id !== undefined)
